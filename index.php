9368b7685b1ad78ab20c06e3dd58a10f1ff0afa1
James Henderson <jameshenderson12@hotmail.com>
2022-10-27 13:01:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -94,7 +94,7 @@ session_start();
 
       <footer class="mt-auto">
         <p>Predictions game based on <a href="https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022" class="text-white">FIFA World Cup Qatar 2022™</a></p>
-        <p><?=$title?><?=$version?> &copy; <?=$year?><?=$developer?>.</p>
+        <p><?=$title?> <?=$version?> &copy; <?=$year?> <?=$developer?>.</p>
       </footer>
 
 	  </div>
