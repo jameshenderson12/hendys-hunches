dcf47e102b4bb602e2b77d88db983c71ccc96d10
James Henderson <jameshenderson12@hotmail.com>
2022-10-28 00:16:04 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -286,10 +286,10 @@
 
         <div class="container text-center g-3">
           <label for="avatar" class="form-label">Select your avatar strip</label>
-          <div class="row row-cols-4 row-cols-sm-6 g-1 btn-group" role="group" aria-label="Basic radio toggle button group">
-            <div class="invalid-feedback">
-              Please select a kit avatar.
-            </div>            
+          <div class="invalid-feedback">
+            Please select a kit avatar.
+          </div>
+          <div class="row row-cols-4 row-cols-sm-6 g-1">
             <div class="col">
               <input type="radio" class="btn-check" autocomplete="off" id="fk1" name="fkradio" value="<?php echo $fk1; ?>" onClick="chooseImage('fk1');">
               <label class="btn btn-outline-light" for="fk1"><img src="<?php echo $fk1; ?>" alt="Football kit description..." class="w-100" /></label>
@@ -494,9 +494,8 @@
       </div>
     </div>
 
-    <footer class="mt-auto text-muted text-small">
-      <p>Predictions game based on <a href="https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022" class="text-white">FIFA World Cup Qatar 2022™</a></p>
-      <p><?=$title?> <?=$version?> &copy; <?=$year?> <?=$developer?>.</p>
+    <footer class="mt-auto">
+      <p class="small fw-lighter">Predictions game based on <a href="https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022" class="text-white">FIFA World Cup Qatar 2022™</a><br><?=$title?> <?=$version?> &copy; <?=$year?> <?=$developer?>.</p>
     </footer>
 		</div>
 
