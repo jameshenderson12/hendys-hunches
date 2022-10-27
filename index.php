fa05a0388dcccaf08c5e06ab012c5f2820531939
James Henderson <jameshenderson12@hotmail.com>
2022-10-27 11:56:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,12 +12,13 @@ session_start();
 		<title>Hendy's Hunches: Login</title>
     <?php include "php/config.php" ?>
 		<link rel="shortcut icon" href="ico/favicon.ico">
-		<link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/cover/">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link href="css/login.css" rel="stylesheet">
+    <!--
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  -->
 	</head>
 
 	<body class="d-flex h-100 text-center text-bg-dark">
