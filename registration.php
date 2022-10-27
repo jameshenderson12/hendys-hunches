d56f5944a65d7ca2ebc71d3ea6c79954b4e7b9c8
James Henderson <jameshenderson12@hotmail.com>
2022-10-28 00:44:01 +0100

    Update registration.php

--- registration.php
+++ registration.php
@@ -16,172 +16,10 @@
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script type="text/javascript">
-    /*
-    	function flagIncorrect() {
-    		$(this).addClass("incorrect");
-    	}
-    	function flagCorrect() {
-    		$(this).addClass("correct");
-    	}
-    	function validateFullForm() {
-    		// Validate the username input
-    		var un = document.forms["registrationForm"]["username"];
-    		var unmsg = document.getElementById("un-msg");
-    		if ((un.value == null) || (un.value == "") || ($('#un-msg').html() != "") ) {
-    			alert("Please enter a unique username.");
-    			un.style.border="1px solid #C33";
-    			un.focus();
-    			return false;
-    		}
-    		// Validate the password input
-    		var pwd1 = document.forms["registrationForm"]["password"];
-    		var pwd2 = document.forms["registrationForm"]["password2"];
-    		if(pwd1.value != "" && pwd1.value == pwd2.value) {
-    		  if(!checkPassword(pwd1.value)) {
-    			alert("The password you have entered is not valid.");
-    			pwd1.focus();
-    			return false;
-    		  }
-    		} else {
-    		  alert("Please check that you've entered and confirmed your password correctly.");
-    		  pwd1.focus();
-    		  return false;
-    		}
-
-    		// Validate the avatar selection
-    		var avs = document.getElementById("avatarSelection");
-    		var avt = document.getElementById("avatars");
-    		if (avs.value == null || avs.value == "") {
-    			alert("Please select a football kit avatar.");
-    			avt.style.border="1px solid #C33";
-    			return false;
-    		}
-    		// Validate the field of work input
-    		var xd1 = document.getElementById("fieldofwork");
-    		var yd1 = document.getElementById("fieldofwork").options;
-    			if (yd1[xd1.selectedIndex].index == "0") {
-    				alert("Please specify your field of work.");
-    				xd1.style.border="1px solid #C33";
-    				xd1.focus();
-    				return false;
-    			}
-    		// Validate the favourite team input
-    		var xd2 = document.getElementById("faveteam");
-    		var yd2 = document.getElementById("faveteam").options;
-    			if (yd2[xd2.selectedIndex].index == "0") {
-    				alert("Please choose your favourite team.");
-    				xd2.style.border="1px solid #C33";
-    				xd2.focus();
-    				return false;
-    			}
-    		// Validate the world cup winner input
-    		var xd3 = document.getElementById("tournwinner");
-    		var yd3 = document.getElementById("tournwinner").options;
-    			if (yd3[xd3.selectedIndex].index == "0") {
-    				alert("Please specify who you think will win the tournament.");
-    				xd3.style.border="1px solid #C33";
-    				xd3.focus();
-    				return false;
-    			}
-    		// Validate the disclaimer checkbox input
-    		var dc = document.forms["registrationForm"]["disclaimer"];
-    		if (!dc.checked) {
-    			alert("You must agree to the terms and conditions.");
-    			dc.focus();
-    			return false;
-    		}
-    	}
-    	// Turn the name fields red if not input (onBlur - focus leaving the field)
-    	function validateName(nameID) {
-    		var x = document.getElementById(nameID);
-    		if (x.value == null || x.value == "") {
-    			x.style.border="1px solid #C33";
-    			return false;
-    		}
-    		else x.style.border="1px solid #090";
-    	}
-    	function checkPassword(str) {
-    	    var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}$/;
-        	return re.test(str);
-      	}
-    	// Turn the password field red if not input correct (onBlur - focus leaving the field)
-    	function validatePassword() {
-    		var x = document.getElementById("password");
-    		if ((x.value == null) || (x.value == "") || (!checkPassword(x.value))) {
-    			x.style.border="1px solid #C33";
-    			return false;
-    		}
-    		else x.style.border="1px solid #090";
-    	}
-    	// Turn the confirm password field red if not input correct (onBlur - focus leaving the field)
-    	function validatePassword2() {
-    		var x = document.getElementById("password2");
-    		var y = document.getElementById("password");
-    		if ((x.value == null) || (x.value == "") || (!checkPassword(x.value)) || (x.value != y.value)) {
-    			x.style.border="1px solid #C33";
-    			return false;
-    		}
-    		else x.style.border="1px solid #090";
-    	}
-    	// Turn the email field red if not input correct (onBlur - focus leaving the field)
-    	function validateEmail() {
-    		var x = document.getElementById("email").value;
-    		var y = document.getElementById("email");
-    		var atpos = x.indexOf("@");
-    		var dotpos = x.lastIndexOf(".");
-    		if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= x.length) {
-    			y.style.border="1px solid #C33";
-    			return false;
-    		}
-    		else
-    		{
-    			y.style.border="1px solid #090";
-    		}
-    	}
-    	// Turn the dropdown fields red if no selection made (onBlur - focus leaving the field)
-    	function validateDropDown(dropDownID) {
-    		var x = document.getElementById(dropDownID);
-    		var y = document.getElementById(dropDownID).options;
-
-    		if (y[x.selectedIndex].index = 0) {
-    			x.style.border="1px solid #C33";
-    			return false;
-    		}
-    		else if (x.selectedIndex > 0) {
-    			x.style.border="1px solid #090";
-    		}
-    		else {
-    			x.style.border="1px solid #C33";
-    		}
-    	}
-    	// Turn the score fields red if not input (onBlur - focus leaving the field)
-    	function validateScore(inputID) {
-    		var x = document.getElementById(inputID);
-    		if (x.value == null || x.value == "") {
-    			x.style.border="1px solid #C33";
-    			return false;
-    		}
-    		else if ((x.value >= 0) && (x.value <= 10)) {
-    			x.style.border="1px solid #090";
-    		}
-    		else x.style.border="1px solid #C33";
-    	}
-    	// Reset all guidance borders to original colour
-    	function resetBorders() {
-    		var x = document.getElementById("registrationForm");
-    		$("button").removeClass("highlight");
-    		for (var i = 0; i < x.length; i++) {
-    			x.elements[i].style.border="1px solid #CCC";
-    		}*/
-    	//}
-  	</script>
-
-    <script>
       $(document).ready(function(){
         $('#username').keyup(username_check);
   		  $('#username').addClass("incorrect");
       });
-
       function username_check(){
       	var username = $('#username').val();
       	if(username == "" || username.length < 4) {
@@ -214,6 +52,16 @@
       	});
       	}
       }
+      function validateAvatar() {
+    		// Validate the avatar selection
+    		var avs = document.getElementById("avatarSelection");
+    		var avt = document.getElementById("avatars");
+    		if (avs.value == null || avs.value == "") {
+    			alert("Please select a football kit avatar.");
+    			avt.style.border="1px solid #C33";
+    			return false;
+        }
+    	}
       </script>
   </head>
 
