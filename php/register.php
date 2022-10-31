a3462719c3f7de94c2c2567e98f6c075cedb6b97
James Henderson <jameshenderson12@hotmail.com>
2022-10-31 23:14:03 +0000

    Update register.php

--- php/register.php
+++ php/register.php
@@ -53,7 +53,7 @@
     <meta name="author" content="James Henderson">
 		<title>Hendy's Hunches: Registration</title>
     <?php include "config.php" ?>
-		<!--<link rel="shortcut icon" href="../ico/favicon.ico">-->
+		<link rel="shortcut icon" href="../ico/favicon.ico">
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link href="../css/registration.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
