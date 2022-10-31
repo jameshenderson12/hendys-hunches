6b96622e89cf7f2ada6637845028919c5686bb58
James Henderson <jameshenderson12@hotmail.com>
2022-10-31 21:13:16 +0000

    Update register.php

--- php/register.php
+++ php/register.php
@@ -55,8 +55,6 @@
     <?php include "config.php" ?>
 		<!--<link rel="shortcut icon" href="../ico/favicon.ico">-->
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
     <link href="../css/registration.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
@@ -73,11 +71,11 @@
       <p>Thank you for signing up to play Hendy's Hunches.</p>
       <p>You will now be automatically redirected back to the login page.</p>
       <p>If you are not redirected automatically, please <a href='../index.php'>click here</a>.</p>
-
+<!--
       <div class="spinner"></div>
 
       <div id="spacer"></div>
-
+-->
 			<footer class="mt-auto">
 	      <p class="small fw-light">Predictions game based on <a href="https://www.fifa.com/fifaplus/en/tournaments/mens/worldcup/qatar2022" class="text-white">FIFA World Cup Qatar 2022™</a><br><?=$title?> <?=$version?> &copy; <?=$year?> <?=$developer?>.</p>
 	    </footer>
