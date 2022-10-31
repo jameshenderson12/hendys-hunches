9482f4b9351d91fe979276ee9e63d4439f897e10
James Henderson <64137708+jameshenderson12@users.noreply.github.com>
2022-10-31 20:52:23 +0000

    Create db-connect.php

---
+++ php/db-connect.php
@@ -0,0 +1,18 @@
+<?php
+	// Setup global variables
+	$dbusername = "hh_jacohe12_adm";
+	$dbpassword = "Q8CwYWmkG8pCwcM";
+	$database = "hh_wc2022_live";
+	$server = "92.205.14.36";
+
+	// Create DB connection
+	$con = mysqli_connect($server, $dbusername, $dbpassword, $database);
+
+	// Check connection
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	else {
+		/*echo "<script type='text/javascript'>alert('Connected to DataBase!');</script>";*/
+	}
+?>
