35299b5556bd48b8ab082b58eb6339d251c63d6b
James Henderson <jameshenderson12@hotmail.com>
2022-11-07 22:33:01 +0000

    Update predictions.php

--- predictions.php
+++ predictions.php
@@ -15,6 +15,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 		<title>Hendy's Hunches: Predictions</title>
     <?php include "php/config.php" ?>
 		<link rel="shortcut icon" href="ico/favicon.ico">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
@@ -55,7 +56,10 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 			}
 		}
 	</script>
-
+	<style>
+	body {
+		font-family: "Trirong" !important;
+	}
   </head>
 
   <body>
@@ -75,27 +79,27 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 		        <div class="offcanvas-body">
 		          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 		            <li class="nav-item">
-		              <a class="nav-link active" aria-current="page" href="#">Home</a>
+		              <a class="nav-link active" aria-current="page" href="dashboard.php">Home</a>
 		            </li>
 		            <li class="nav-item">
-		              <a class="nav-link" href="#">My Predictions</a>
+		              <a class="nav-link" href="predictions.php">My Predictions</a>
 		            </li>
 								<li class="nav-item">
-		              <a class="nav-link" href="#">Rankings</a>
+		              <a class="nav-link" href="rankings.php">Rankings</a>
 		            </li>
 								<li class="nav-item">
-		              <a class="nav-link" href="#">How It Works</a>
+		              <a class="nav-link" href="howitworks.php">How It Works</a>
 		            </li>
 								<li class="nav-item">
-									<a class="nav-link" href="#">About</a>
+									<a class="nav-link" href="about.php">About</a>
 								</li>
 
 		            <li class="nav-item dropdown">
 		              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-		                <img src="img/scores.jpg" alt="Profile icon" class="img-fluid rounded" width="20px;">
+		                <img src="img/scores.jpg" alt="Profile icon" class="img-fluid rounded mr-2" width="20px;">
 										<?php
 											// Echo session variables that were set on previous page
-											echo $_SESSION["firstname"];
+											echo $_SESSION["firstname"] $_SESSION["surname"];
 										?>
 		              </a>
 		              <ul class="dropdown-menu">
