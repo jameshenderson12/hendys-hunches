b45ba77774a6e44d3df408e3ed7a9413e420c027
James Henderson <jameshenderson12@hotmail.com>
2022-11-11 23:24:34 +0000

    Font updates

--- about.php
+++ about.php
@@ -15,7 +15,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 		<title>Hendy's Hunches: About</title>
     <?php include "php/config.php" ?>
 		<link rel="shortcut icon" href="ico/favicon.ico">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 		<link rel="stylesheet" href="css/default.css">
@@ -126,7 +126,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 						    <li class="list-group-item"><strong>3rd:</strong> Steve Butt/Kirsty Yarnold</li>
 						  </ul>
 						</div>
-					</div>										
+					</div>
 		    </div>
 
 		    <a class="btn btn-default" href="#top" role="button">Return to top</a>
--- howitworks.php
+++ howitworks.php
@@ -15,7 +15,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 		<title>Hendy's Hunches: How It Works</title>
     <?php include "php/config.php" ?>
 		<link rel="shortcut icon" href="ico/favicon.ico">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 		<link rel="stylesheet" href="css/default.css">
