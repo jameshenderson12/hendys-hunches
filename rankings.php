88860ac4263937a374ed402f12c3cb0f92c72d70
James Henderson <jameshenderson12@hotmail.com>
2022-11-16 21:29:01 +0000

    Project icon changes

--- rankings.php
+++ rankings.php
@@ -27,7 +27,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
   <body>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Offcanvas navbar large">
 		    <div class="container">
-					<img src="img/hh-favicon-2018.jpg" class="img-responsive" style="margin: 0px 20px 0px; height:50px">
+					<img src="ico/favicon.ico" class="img-fluid bg-light rounded mx-2" width="50px">
 		      <a class="navbar-brand" href="#">Hendy's Hunches</a>
 		      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
 		        <span class="navbar-toggler-icon"></span>
