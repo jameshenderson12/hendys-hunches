ffceb2c0ddf978fbabd2d9db0a62e94cfe7db46d
James Henderson <jameshenderson12@hotmail.com>
2022-11-18 15:05:03 +0000

    User updated

--- user.php
+++ user.php
@@ -236,13 +236,11 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 																	fixture += '<td class="small text-muted d-none d-md-block">' + value.Group + '</td>';
 			                            fixture += '<td>' + value.HomeTeam + '</td>';
 																	fixture += '<td><img src="' + homeTeamFlag + '" alt="Flag of ' + homeTeam + '" title="Flag of ' + homeTeam + '"></td>';
-																	fixture += '<td><input type="text" id="score' + x + '_p" name="score' + x + '_p" class="form-control" /></td>';
 																	fixture += '<td align="center">v<br><span class="badge bg-light text-primary">' + value.MatchNumber + '</span></td>';
-																	fixture += '<td><input type="text" id="score' + y + '_p" name="score' + y + '_p" class="form-control" /></td>';
 																	fixture += '<td><img src="' + awayTeamFlag + '" alt="Flag of ' + awayTeam + '" title="Flag of ' + awayTeam + '"></td>';
 			                            fixture += '<td>' + value.AwayTeam + '</td>';
 			                            fixture += '<td class="small text-muted d-none d-md-block">' + date + '</td>';
-																	fixture += '<td align="center"><span class="prediction"><?php echo $userdata['"score' + x + '_p"'] ?> - <?php echo $userdata['"score' + y + '_p"'] ?></span></td>';
+																	fixture += '<td align="center"><span class="prediction"><?php echo $userdata['score' + x + '_p'] ?> - <?php echo $userdata['score' + y + '_p'] ?></span></td>';
 													        fixture += '<td align="center"><?php if($matchids[0]) { printf ("<span class='result'>%s - %s</span>", $matchresult["score".$oddgameno[0]."_r"], $matchresult["score".$evengameno[0]."_r"]); } else echo "N / A"; ?></td>';
 													      	fixture += '<td align="center"><?php if($matchids[0]) { echo $matchpoints[0]; } else { echo "-"; } ?></td>';
 			                            fixture += '</tr>';
