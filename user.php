2905e18a7f31b000797ce2d4c2d4f57695e3f416
James Henderson <jameshenderson12@hotmail.com>
2022-11-18 16:58:43 +0000

    Updates

--- user.php
+++ user.php
@@ -177,9 +177,9 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 	      <div class="row">
 					<div class="col-md-3">
 					<div class="card">
-					  <img src="<?php echo $avatar ?>" id="avatar" class="img-fluid mx-auto p-2" alt="User Avatar" name="User Avatar" width="120">
+					  <img src="<?php echo $avatar ?>" id="avatar" class="img-fluid mx-auto p-2" alt="User Avatar" name="User Avatar" width="100">
 					  <div class="card-body">
-					    <h5 class="card-title" style="text-align: center; font-weight: bolder;"><?php printf("%s<span class='mx-2' style='color:#CCC;'>|</span>%s pts", $currentpos, $pointstotal); ?></h5>
+					    <h5 class="card-title" style="text-align: center; font-weight: bolder; margin-top:-10px;"><?php printf("%s<span class='mx-2' style='color:#CCC;'>|</span>%s pts", $currentpos, $pointstotal); ?></h5>
 					    <!--<p class="card-text"><?php printf ("%s thinks %s will win FIFA World Cup 2022.", $uppCaseFN, $tournwinner); ?></p>-->
 					  </div>
 					  <ul class="list-group list-group-flush">
