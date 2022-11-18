d4145041c2200a63002aad95056e0449f7475999
James Henderson <jameshenderson12@hotmail.com>
2022-11-18 12:08:51 +0000

    Fixture added into user page

--- user.php
+++ user.php
@@ -195,6 +195,30 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 				</div>
 				<div class="col-md-9">
 					<div class="card">
+						<div class="card-body">
+							<table class="table table-striped" style="background-color: #FFF;">
+			        <tr>
+				        <th width="6%"></th>
+				        <th width="22%"></th>
+				        <th width="10%"></th>
+				        <th width="22%"></th>
+				        <th align="center" width="">Prediction</th>
+				        <th align="center" width="">Result</th>
+				        <th align="center" width="10%">Points</th>
+			        </tr>
+			      	<tr>
+								<td class="date-venue">Match 1<br>Group A</td>
+				    		<td class="left-team">
+				        <img src="<?php echo $A1img; ?>" alt="<?php echo $A1; ?>" title="<?php echo $A1; ?>"><label for="score1_p"><?php echo $A1; ?></label></td>
+				        <td align="center"><span>v</span></td>
+								<td class="right-team">
+				        <img src="<?php echo $A2img; ?>" alt="<?php echo $A2; ?>" title="<?php echo $A2; ?>"><label for="score2_p"><?php echo $A2; ?></label></td>
+				      	<td align="center"><span class="prediction"><?php echo $userdata['score1_p'] ?> - <?php echo $userdata['score2_p'] ?></span></td>
+				        <td align="center"><?php if($matchids[0]) { printf ("<span class='result'>%s - %s</span>", $matchresult["score".$oddgameno[0]."_r"], $matchresult["score".$evengameno[0]."_r"]); } else echo "N / A"; ?></td>
+				      	<td align="center"><?php if($matchids[0]) { echo $matchpoints[0]; } else { echo "-"; } ?></td>
+			      	</tr>
+							</table>
+						</div>
 					</div>
 				</div>
       	</div><!--row-->
