43fbf836eae4d679fa337aae3845d76978acae67
James Henderson <jameshenderson12@hotmail.com>
2022-11-18 11:46:27 +0000

    Update user.php

--- user.php
+++ user.php
@@ -179,10 +179,10 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 					<div class="card">
 					  <img src="<?php echo $avatar ?>" id="avatar" class="img-fluid mx-auto" alt="User Avatar" name="User Avatar" width="120">
 					  <div class="card-body">
-					    <h5 class="card-title mx-auto"><?php printf("<span class='mx-2' style='color:#CCC;'>|</span>%s pts", $currentpos, $pointstotal); ?></h5>
+					    <h5 class="card-title mx-auto"><?php printf("%s<span class='mx-2' style='color:#CCC;'>|</span>%s pts", $currentpos, $pointstotal); ?></h5>
 					    <p class="card-text"><?php printf ("%s thinks %s will win FIFA World Cup 2022.", $uppCaseFN, $tournwinner); ?></p>
 					  </div>
-					  <ul class="list-group list-group-flush">					    
+					  <ul class="list-group list-group-flush">
 					    <li class="list-group-item"><?php printf ("<strong>Favourite Team:</strong><br> %s", $faveteam); ?></li>
 					    <li class="list-group-item"><?php printf ("<strong>Location:</strong><br> %s", $location); ?></li>
 							<li class="list-group-item"><?php printf ("<strong>Field of Work:</strong><br> %s", $fieldofwork); ?></li>
