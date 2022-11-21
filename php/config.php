37d5ea622200314c7496e02bff4184e49251dc5e
James Henderson <jameshenderson12@hotmail.com>
2022-11-21 13:52:37 +0000

    Update config.php

--- php/config.php
+++ php/config.php
@@ -281,7 +281,7 @@ function checkSubmitted() {
 	include 'php/db-connect.php';
 
 	// Get team information from the DB	counting occurrences too
-	$sql_predstatus = "SELECT EXISTS (SELECT username FROM live_user_predictions_groups WHERE username = '. $_SESSION["username"] . ')";
+	$sql_predstatus = "SELECT EXISTS(SELECT username FROM live_user_predictions_groups WHERE username = '".$_SESSION["username"]."') ";
 	$predstatus = mysqli_query($con, $sql_predstatus);
 	if ($predstatus = 1) {
 		consoleMsg($predstatus);
