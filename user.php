91cc46f23f0c1c80ac0846a2487af209daa0c874
James Henderson <jameshenderson12@hotmail.com>
2022-11-21 14:58:44 +0000

    Update user.php

--- user.php
+++ user.php
@@ -222,8 +222,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 			                        var fixture = '';
 															var	x = 1;
 															var y = 2;
-															var z = 0;
-															?>
+															var z = 0;														
 			                        // Iterate through objects
 			                        $.each(data, function (key, value) {
 																	var homeTeam = value.HomeTeam;
@@ -243,9 +242,9 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 																	fixture += '<td><img src="' + awayTeamFlag + '" alt="Flag of ' + awayTeam + '" title="Flag of ' + awayTeam + '"></td>';
 			                            fixture += '<td>' + value.AwayTeam + '</td>';
 			                            fixture += '<td class="small text-muted d-none d-md-block">' + date + '</td>';
-																	//fixture += '<td align="center"><span class="prediction"><?php echo $userdata["score".$x."_p"]; ?> - <?php echo $userdata["score".$y."_p"] ?></span></td>';
-													        //fixture += '<td align="center"><?php if($matchids[z]) { printf ("<span class='result'>%s - %s</span>", $matchresult["score".$oddgameno[z]."_r"], $matchresult["score".$evengameno[z]."_r"]); } else echo "N / A"; ?></td>';
-													      	//fixture += '<td align="center"><?php if($matchids[z]) { echo $matchpoints[z]; } else { echo "-"; } ?></td>';
+																	fixture += '<td align="center"><span class="prediction"><?php echo $userdata["score".$x."_p"]; ?> - <?php echo $userdata["score".$y."_p"] ?></span></td>';
+													        fixture += '<td align="center"><?php if($matchids[z]) { printf ("<span class='result'>%s - %s</span>", $matchresult["score".$oddgameno[z]."_r"], $matchresult["score".$evengameno[z]."_r"]); } else echo "N / A"; ?></td>';
+													      	fixture += '<td align="center"><?php if($matchids[z]) { echo $matchpoints[z]; } else { echo "-"; } ?></td>';
 			                            fixture += '</tr>';
 																	x+=2;
 																	y+=2;
