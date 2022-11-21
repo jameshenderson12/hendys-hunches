a0d41116434a0e52d7b344d7dc187e2663a3533c
James Henderson <jameshenderson12@hotmail.com>
2022-11-21 15:37:07 +0000

    Update user.php

--- user.php
+++ user.php
@@ -215,12 +215,13 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 				        </tr>
 							-->
 								<tr>
-									<td class="date-venue">Match 1<br>Group A</td>
-								  <td class="left-team">
-							    <img src="<?php echo $A1img; ?>" alt="<?php echo $A1; ?>" title="<?php echo $A1; ?>"><label for="score1_p"><?php echo $A1; ?></label></td>
+									<td class="small text-muted d-none d-md-block">Match 1<br>Group A</td>
+								  <td style="text-align: right"><label for="score1_p"><?php echo $A1; ?></label></td>
+							    <td><img src="<?php echo $A1img; ?>" alt="<?php echo $A1; ?>" title="<?php echo $A1; ?>"></td>
 							    <td align="center"><span>v</span></td>
-									<td class="right-team">
-							    <img src="<?php echo $A2img; ?>" alt="<?php echo $A2; ?>" title="<?php echo $A2; ?>"><label for="score2_p"><?php echo $A2; ?></label></td>
+									<td><img src="<?php echo $A2img; ?>" alt="<?php echo $A2; ?>" title="<?php echo $A2; ?>"></td>
+									<td class="right-team"><label for="score2_p"><?php echo $A2; ?></label></td>
+									<td class="small text-muted d-none d-md-block">21/11/2022</td>
 							    <td align="center"><span class="prediction"><?php echo $userdata['score1_p'] ?> - <?php echo $userdata['score2_p'] ?></span></td>
 							    <td align="center"><?php if($matchids[0]) { printf ("<span class='result'>%s - %s</span>", $matchresult["score".$oddgameno[0]."_r"], $matchresult["score".$evengameno[0]."_r"]); } else echo "N/A"; ?></td>
 							    <td align="center"><?php if($matchids[0]) { echo $matchpoints[0]; } else { echo "-"; } ?></td>
