18de05ec7fea680397b4d376b8f5cc9558624457
James Henderson <jameshenderson12@hotmail.com>
2022-11-22 22:10:00 +0000

    Update dashboard-items.php

--- php/dashboard-items.php
+++ php/dashboard-items.php
@@ -548,7 +548,7 @@ function displayPersonalInfo() {
 		printf("<div class='col-md-2'><i class='bi bi-person-workspace'></i></div><div class='col-md-10'>Works in %s</div>", $fieldofwork);
 		printf("<div class='col-md-2'><i class='bi bi-lightbulb'></i></div><div class='col-md-10'>Thinks %s will win</div>", $tournwinner);
 		printf("<div class='col-md-2'><i class='bi bi-calendar'></i></div><div class='col-md-10'>Signed up %s</div>", $convertedDate);
-		printf("<div class='col-md-2'><i class='bi bi-credit-card-2-front'></i></div><div class='col-md-10'>Fee paid? %s</div>", $haspaid);
+		//printf("<div class='col-md-2'><i class='bi bi-credit-card-2-front'></i></div><div class='col-md-10'>Fee paid? %s</div>", $haspaid);
 		print("</div><hr>");
 		printf("<p class='text-center mt-2'><a href='user.php?id=%s' title='Show predictions'>View My Predictions</a></p>", $_SESSION['id']);
 		print("<p class='text-center mt-1'><a href='change-password.php'>Change password</a></p>");
