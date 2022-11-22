35438d840a5d8d67bbe462a9126b8c79ecc93993
James Henderson <jameshenderson12@hotmail.com>
2022-11-22 14:00:18 +0000

    Update dashboard-test.php

--- dashboard-test.php
+++ dashboard-test.php
@@ -18,7 +18,8 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Ubuntu|Lora">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-		<link rel="stylesheet" href="css/default.css">
+		<link rel="stylesheet" href="css/custom.css">
+		<link class="include" rel="stylesheet" type="text/css" href="css/jquery.jqplot.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 		<?php include 'php/dashboard-items.php'; ?>
