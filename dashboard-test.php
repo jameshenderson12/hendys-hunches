2c4dd2582fbaec26925a6e3cd131523728ef103a
James Henderson <jameshenderson12@hotmail.com>
2022-11-22 14:06:37 +0000

    Update dashboard-test.php

--- dashboard-test.php
+++ dashboard-test.php
@@ -9,6 +9,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 <html lang="en" class="h-100">
   <head>
     <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Hendy's Hunches: Predictions Game">
     <meta name="author" content="James Henderson">
@@ -24,6 +25,11 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
     <!--<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 		<?php include 'php/dashboard-items.php'; ?>
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->		
   </head>
 
   <body>
