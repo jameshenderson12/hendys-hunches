24028f8a4048c9d0b5bb828feaad9a42839c5422
James Henderson <jameshenderson12@hotmail.com>
2022-11-22 23:25:59 +0000

    Update dashboard-test.php

--- dashboard-test.php
+++ dashboard-test.php
@@ -8,8 +8,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 <!DOCTYPE html>
 <html lang="en" class="h-100">
   <head>
-    <meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">		
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Hendy's Hunches: Predictions Game">
     <meta name="author" content="James Henderson">
@@ -20,16 +19,9 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 		<link rel="stylesheet" href="css/default.css">
-		<!--<link class="include" rel="stylesheet" type="text/css" href="css/jquery.jqplot.min.css" />-->
-		<!--<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.2.min.js"></script>-->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 		<?php include 'php/dashboard-items.php' ?>
-		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
