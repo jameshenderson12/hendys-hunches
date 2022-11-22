1052d32abe8b888ff2be22559f63b9d776897cc1
James Henderson <jameshenderson12@hotmail.com>
2022-11-22 00:17:26 +0000

    Various minor updates

--- dashboard-test.php
+++ dashboard-test.php
@@ -21,6 +21,7 @@ if (!(isset($_SESSION['login']) && $_SESSION['login'] != "")) {
 		<link rel="stylesheet" href="css/default.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+		<?php include 'php/dashboard-items.php'; ?>
   </head>
 
   <body>
