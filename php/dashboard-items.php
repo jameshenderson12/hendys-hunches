acecc21767f7a551de677e867e28aa25df299eea
James Henderson <jameshenderson12@hotmail.com>
2022-12-03 00:40:48 +0000

    Update dashboard-items.php

--- php/dashboard-items.php
+++ php/dashboard-items.php
@@ -542,9 +542,9 @@ function displayGroupMatchesPlayed() {
 function checkSubmitted() {
 	// Create DB connection
 	include 'db-connect.php';
-	$un = $_SESSION["username"];
+	$un = mysql_real_escape_string($_SESSION["username"]);
 	// Get team information from the DB	counting occurrences too
-	$sql_predstatus = printf("SELECT EXISTS SELECT username FROM live_user_predictions_ro16 WHERE username = '%s'", $un);
+	$sql_predstatus = printf("SELECT EXISTS ("SELECT username FROM live_user_predictions_ro16 WHERE username = '%s'")", $un);
 	$predstatus = mysqli_query($con, $sql_predstatus);
 	consoleMsg($predstatus);
 
