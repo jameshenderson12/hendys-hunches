f632d0b4817c5ba1ab7972ef8c02f7e390757c68
James Henderson <jameshenderson12@hotmail.com>
2022-12-04 00:11:13 +0000

    Update dashboard-items.php

--- php/dashboard-items.php
+++ php/dashboard-items.php
@@ -524,8 +524,8 @@ function displayMatchesRecorded() {
 
 function displayGroupMatchesPlayed() {
 	// Create DB connection
-	include 'php/db-connect.php';
-
+	//include 'php/db-connect.php';
+/*
 	$sql_get_matches_played = "SELECT COUNT(*) AS matches_played FROM live_match_results";
 	$matches_played = mysqli_query($con, $sql_get_matches_played);
 
@@ -533,10 +533,10 @@ function displayGroupMatchesPlayed() {
 		$no_of_matches_played = $row["matches_played"];
 		//console.log($no_of_matches_played);
 		$percent_group_played = round($no_of_matches_played * 100 / 48);
-	}
-	print("<div class='progress'><div class='progress-bar bg-success' role='progressbar' aria-label='Competition progress bar' style='width: $percent_group_played%;' aria-valuenow='$percent_group_played' aria-valuemin='0' aria-valuemax='100'>$percent_group_played%</div></div>");
+	}*/
+	print("<div class='progress'><div class='progress-bar bg-success' role='progressbar' aria-label='Competition progress bar' style='width: 100%;' aria-valuenow='100' aria-valuemin='0' aria-valuemax='100'>100%</div></div>");
 	// Close DB connection
-	mysqli_close($con);
+	//mysqli_close($con);
 }
 
 function checkSubmitted() {
@@ -569,8 +569,8 @@ function displayRO16MatchesPlayed() {
 		//console.log($no_of_matches_played);
 		$percent_ro16_played = round($no_of_ro16_matches_played * 100 / 48);
 	}
-	//print("<div class='progress'><div class='progress-bar' role='progressbar' aria-label='Competition progress bar' style='width: $percent_ro16_played%;' aria-valuenow='$percent_ro16_played' aria-valuemin='0' aria-valuemax='100'>$percent_ro16_played%</div></div>");
-	print("<div class='progress'><div class='progress-bar' role='progressbar' aria-label='Competition progress bar' style='width: 0%;' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100'></div></div>");
+	print("<div class='progress'><div class='progress-bar' role='progressbar' aria-label='Competition progress bar' style='width: $percent_ro16_played%;' aria-valuenow='$percent_ro16_played' aria-valuemin='0' aria-valuemax='100'>$percent_ro16_played%</div></div>");
+	//print("<div class='progress'><div class='progress-bar' role='progressbar' aria-label='Competition progress bar' style='width: 0%;' aria-valuenow='0' aria-valuemin='0' aria-valuemax='100'></div></div>");
 	// Close DB connection
 	mysqli_close($con);
 }
