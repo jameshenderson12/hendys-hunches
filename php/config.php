451a409096e2bc2ed198f5228fa28dae7760cade
James Henderson <64137708+jameshenderson12@users.noreply.github.com>
2026-01-15 21:24:04 +0000

    Update config.php

--- php/config.php
+++ php/config.php
@@ -17,6 +17,7 @@
 // ini_set('html_errors', 1);
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
+define('IS_PREVIEW', true);
 
 //===============================
 // Global variables
