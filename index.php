174624fa3794c11b9a3a29a71f1f1da4a7337577
James Henderson <64137708+jameshenderson12@users.noreply.github.com>
2026-01-15 20:11:12 +0000

    Change button to link for registration navigation

--- index.php
+++ index.php
@@ -83,9 +83,9 @@ session_start();
                         <button type="submit" class="btn btn-primary w-100 mt-0"><i class="fw-bold bi bi-box-arrow-in-right"></i> Log in</button>
                       </div>
 					  <div class="col-12 col-md-7 mt-3">
-						  <button class="btn w-100 text-black" style="background-color: var(--primary-color)">
+						  <a class="btn w-100 text-black" href="registration.php" style="background-color: var(--primary-color)">
 					  		  <i class="bi bi-person-fill-add"></i> Register
-						  </button>
+						  </a>
 					  </div>
                       <div class="col-12 col-md-5">                        
                         <div class="text-end small mt-4">
